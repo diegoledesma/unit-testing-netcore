39eeacd33701427175d3483a522c7429637623a1
Diego Ledesma <diego.ledesma@changepoint.com>
2018-10-10 08:54:43 -0300

    Display albums using stub object

--- react-app/src/Album.js
+++ react-app/src/Album.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Album extends Component {
     render(){
-        return (<div></div>)
+        return (
+        <div style={{border:"1px solid", display:"flex", flexDirection:"column"}}>
+            <span>{this.props.title}</span>
+            <img src={this.props.coverHttpPath} alt="cover"/>
+        </div>)
     }
 
+}
+
+Album.propTypes = {
+    title: PropTypes.string,
+    coverHttpPath: PropTypes.string
 }
 
 export default Album
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import React, { Component } from 'react'
+import AlbumList from './AlbumList'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <AlbumList albums={albumList}></AlbumList>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+var albumList = [
+  {
+     "title":"Hughes / Thrall",
+     "coverHttpPath":"https://img.discogs.com/iNbhVbA4VqFkBgWmsvf1GfHnyfY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-3151392-1445932154-9526.jpeg.jpg"
+  },
+  {
+     "title":"Rattle And Hum Vol.1",
+     "coverHttpPath":"https://img.discogs.com/0Zxh5HAFinsbLCM3msCn_7wKrLA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-12638188-1539100768-5354.jpeg.jpg"
+  },
+  {
+     "title":"Shivers",
+     "coverHttpPath":"https://img.discogs.com/c9iKVGEhxA-wimvYqPzDweC8bIM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-12638152-1539099990-2223.jpeg.jpg"
+  },
+  {
+     "title":"Zombie",
+     "coverHttpPath":"https://img.discogs.com/oJmbBENnpzf3cJcOOlA1JcUncjM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1888854-1250324114.jpeg.jpg"
+  }
+]
+
+export default App
