3f171e5690e17ae26f2640bc7efa2e4be105b49c
Diego Ledesma <diego.ledesma@changepoint.com>
2018-10-10 08:28:11 -0300

    kickstart

---
+++ react-app/src/AlbumList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Album from './Album'
+
+class AlbumList extends Component {
+    render() {
+        return (<div></div>)
+    }
+}
+
+export default AlbumList
