3026ce099110d79448197adfc09b5faaf48f1b93
Diego Ledesma <diego.ledesma@changepoint.com>
2018-10-12 15:17:46 -0300

    Add albums list to album repository tests

--- netcore/UnitTests/AlbumsAPI.Tests/AlbumRepository.Tests.cs
+++ netcore/UnitTests/AlbumsAPI.Tests/AlbumRepository.Tests.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using AlbumsAPI.Repositories;
+using Loggers;
 
 namespace UnitTests.AlbumsAPI.Tests
 {
     [TestClass]
     public class AlbumRepositoryTests
     {
+        private List<Album> _albums;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
         public AlbumRepositoryTests()
         {
+            _albums = new List<Album>
+            {
+                new Album()
+                {
+                    Title = "t1",
+                    CoverHttpPath = "c1"
+                },
+
+                new Album()
+                {
+                    Title = "t2",
+                    CoverHttpPath = "c2"
+                }
+            };
         }
 
         [TestMethod]
