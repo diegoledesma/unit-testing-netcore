d4d3c34c7c86dee3d72e76a47ddec75c79bcd8f6
Diego Ledesma <diego.ledesma@changepoint.com>
2018-10-12 14:59:48 -0300

    add reference for controller tests

--- netcore/UnitTests/AlbumsAPI.Tests/AlbumsController.Tests.cs
+++ netcore/UnitTests/AlbumsAPI.Tests/AlbumsController.Tests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using AlbumsAPI.Controllers;
+using AlbumsAPI.Repositories;
 
 namespace UnitTests.AlbumsAPI.Tests
 {
